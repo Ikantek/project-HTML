596061605f69a094f47fb8efb493fd13ca5d6c29
Ikantek <mczadlo@gmail.com>
2019-12-09 21:12:20 +0100

    changes

---
+++ php/createBlog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <!-- Mobile prepare, viewport is the area of the user's screen, content tells us that the width of the page is the device's width and the content will be pritned with 1.0 scale  -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dodawanie bloga!</title>
+    </head>
+    <body>
+    <?php include 'menu.php'; ?>
+
+    <form action="newBlogFile.php" method="Post" enctype="multipart/form-data">
+        <h1>Formularz dodawania nowego bloga!</h1>
+
+        <div> Nazwa bloga: <input type="text" name="blogName"></div>
+        <div> Nazwa u¿ytkownika: <input type="text" name="userName"></div>
+        <div> Has³o u¿ytkownika: <input type="password" name="userPassword"></div>
+        <div> Opis bloga: <textarea name="blogName" rows="20" cols="30"/></div>
+
+        <div><input type="reset" value="Wyczyœæ pola" name="wyczysc"/></div>
+        <div><input type="submit" value="Stwórz bloga!"></div>
+    </form>
+    </body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,5 @@
+   <a href="createBlog.php">Stwórz bloga</a>
+   <a href="createPost.php">Dodaj wpis</a>
+   <a href="createComment.php">Dodaj komentarz</a>
+   <a href="blogs.php">Przegl¹daj blogi</a>
+   <br /><br />
