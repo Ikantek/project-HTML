4651576758f704cbe9d6d79d588a220672a298a8
Ikantek <mczadlo@gmail.com>
2019-12-10 12:08:01 +0100

    change

--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,5 @@
    <a href="createBlog.php">Create blog</a>
    <a href="createPost.php">Create post</a>
    <a href="createComment.php">Create comment</a>
-   <a href="blogs.php">Browse blogs</a>
+   <a href="blog.php">Browse blogs</a>
    <br /><br />
