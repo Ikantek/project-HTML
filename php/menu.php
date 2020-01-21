7567e419c7f4f078ecb25767939179e4b74f7a27
Ikantek <mczadlo@gmail.com>
2020-01-21 08:12:10 +0100

    asd

--- php/menu.php
+++ php/menu.php
@@ -2,4 +2,5 @@
    <a href="createPost.php">Create post</a>
    <a href="createComment.php">Create comment</a>
    <a href="blog.php">Browse blogs</a>
+   <a href="chat.php">Chat</a>"
    <br /><br />
