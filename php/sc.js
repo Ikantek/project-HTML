182e93d3b8f9307a19b162b750716ce6d0b2bbd1
Ikantek <mczadlo@gmail.com>
2020-01-21 05:39:59 +0100

    xd

--- php/sc.js
+++ php/sc.js
@@ -46,7 +46,7 @@ function validateHour() {
 
 function listOfAttachments() {
     var attachments = document.getElementById('attachments');
-    var numberOfLines = attachments.children.length; //ilosc linii w divie
+    var numberOfLines = attachments.children.length;
     var fileNo = numberOfLines / 2; 
 
     var input = document.createElement('input');
@@ -57,73 +57,4 @@ function listOfAttachments() {
     attachments.appendChild(input);
     attachments.appendChild(document.createElement("br"));
 
-}
-
-function stworzStyle() {
-    var style = document.getElementsByTagName("link");
-    var iloscStyli = style.length;
-    console.log(iloscStyli);
-
-    var spisTresci = document.getElementById("miejsce_na_style");
-
-    for (i = 0; i < iloscStyli; i++) {
-        var aktualnyElement = document.createElement('a');
-        var nazwaStylu = style[i].title;
-
-        aktualnyElement.innerHTML = nazwaStylu;
-        aktualnyElement.setAttribute("onclick", "wybierzStyl(\"" + nazwaStylu + "\")");
-
-        console.log("AA");
-        spisTresci.appendChild(aktualnyElement);
-        spisTresci.appendChild(document.createElement('br'));
-    }
-
-
-}
-
-function wybierzStyl(nazwaStylu) {
-    var listaStyli = document.getElementsByTagName("link");
-    var iloscStyli = listaStyli.length;
-    for (var i = 0; i < iloscStyli; i++) {
-        var styl = listaStyli[i];
-        if (styl.getAttribute("title") == nazwaStylu) {
-            styl.disabled = false;
-            console.log(styl.getAttribute("title") + " enabled");
-        } else {
-            styl.disabled = true;
-            console.log(styl.getAttribute("title") + " disabled");
-        }
-    }
-
-    ustawCiasteczko("style", nazwaStylu, 365);
-}
-
-function sprawdzCiasteczko() {
-    var ciasteczko = wczytajCiasteczko("style");
-    wybierzStyl(ciasteczko);
-}
-
-function ustawCiasteczko(nazwa, styl, dni) {
-    var data = new Date();
-    data.setTime(data.getTime() + (dni * 24 * 60 * 60 * 1000));
-    var wygasa = "expires=" + data.toUTCString();
-    document.cookie = nazwa + "=" + styl + ";" + wygasa + "; path=/";
-}
-
-function wczytajCiasteczko(nazwa) {
-    var styl = "";
-    var nazwa = nazwa + "=";
-    var ciasteczko = decodeURIComponent(document.cookie);
-    var ciasteczko_tablica = ciasteczko.split(';');
-    for (var i = 0; i < ciasteczko_tablica.length; i++) {
-        var fragment = ciasteczko_tablica[i];
-        while (fragment.charAt(0) == ' ') {
-            fragment = fragment.substring(1);
-        }
-        if (fragment.indexOf(nazwa) == 0) {
-            styl = fragment.substring(nazwa.length, fragment.length);
-            break;
-        }
-    }
-    return styl;
 }
---
+++ php/styles.js
@@ -0,0 +1,55 @@
+var styles = document.querySelectorAll('link[type="text/css"][media="screen"]');
+var stylesList = document.querySelector('.js-styles-list');
+
+/**
+ * 5. U¿ywaj¹c DOM wygeneruj listê dostêpnych stylów alternatywnych na ka¿dej stronie swojego portalu z
+ * poprzednich æwiczeñ. Lista mo¿e byæ zbudowana np. z elementów li, albo a albo list rozwijanych formularza.
+ */
+for (var i = 0; i < styles.length; i++) {
+    var style = styles[i];
+    var element = document.createElement('li');
+
+    element.innerHTML = style.getAttribute('title');
+    /**
+     * 6. Korzystaj¹c z onClick na elementach listy i DOM
+     * zaprogramuj stronê tak aby przegl¹darka prze³¹cza³a aktywny styl.
+     */
+    element.setAttribute('onclick', 'setStyleActive(' + i + ')');
+    stylesList.appendChild(element);
+}
+
+function setStyleActive(index) {
+    console.log('set active: ', index, styles);
+
+    // Najpierw trzeba wy³¹czyæ wszystkie, bo inaczej przy
+    // odœwie¿eniu strony i próbie ustawienia stylu z cookies coœ siê
+    // buguje i nie dzia³a i smuteczek ogólnie
+    for (var i = 0; i < styles.length; i++) {
+        styles[i].disabled = true;
+    }
+
+    styles[index].disabled = false;
+
+    /**
+     * Ustaw cookie
+     */
+    document.cookie = 'style=' + index + ';';
+}
+
+/**
+ * 7. Za pomoc¹ Cookies zaprogramuj aby wybrany styl by³ domyœlny
+ * przy ponownym odwiedzaniu strony, albo przejœciu do innej strony Twojego portalu.
+ */
+window.addEventListener('load', function () {
+    if (document.cookie) {
+        var cookies = document.cookie.split(/; */);
+
+        for (var i = 0; i < cookies.length; i++) {
+            var data = cookies[i].split('=');
+
+            if (data[0] == 'style') {
+                setStyleActive(parseInt(data[1]));
+            }
+        }
+    }
+});
